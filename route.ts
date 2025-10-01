50f718dc39c3ff6755a0ae3bdf574544fdb2eaa3
parallelqt25 <parallelquantum25@gmail.com>
2025-10-01 16:23:49 +0900

    Create route.ts

---
+++ route.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+import keywordRoutes from './keyword.routes';
+import scanRoutes from './scan.routes';
+
+const router = Router();
+
+router.use('/keywords', keywordRoutes);
+router.use('/', scanRoutes);
+
+export default router;
